95b673be62f5212b8356520ac37447db7a10e17c
Aakash Karlekar <akarlekar99@gmail.com>
2022-08-08 16:00:24 -0400

    Created Top Frame and Side Frame

--- minesweeper.py
+++ minesweeper.py
@@ -3,12 +3,27 @@ from tkinter import *
 game = Tk()
 
 #Settings of the Window
-game.configure(bg='green')
-game.geometry('1440x720')
-game.title('Minesweeper')
-game.resizable(False, False)
+game.configure(bg='green') #Color of the Background
+game.geometry('1440x720') #Size of the Window
+game.title('Minesweeper') #Title of the Window
+game.resizable(False, False) #Window not resizable
 
+top_frame = Frame(
+    game,
+    bg = 'white', #Color of TopFrame
+    width = 1440, #Width of window
+    height = 180 #Height of window
+)
+top_frame.place(x=0, y=0) # Location of Left Corner of window for Top Frame
 
+side_frame = Frame(
+    game,
+    bg = 'white',
+    width = 360,
+    height = 540
+)
+
+side_frame.place(x=0, y=180)
 
 #Code that Runs the Window
 game.mainloop()
