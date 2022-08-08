1e1be22d22b8856f84a4c48b04392ecd39c1d56d
Aakash Karlekar <akarlekar99@gmail.com>
2022-08-08 15:49:50 -0400

    Made code more readable

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,14 @@
 from tkinter import *
 
 game = Tk()
+
+#Settings of the Window
 game.configure(bg='green')
 game.geometry('1440x720')
 game.title('Minesweeper')
 game.resizable(False, False)
+
+
+
+#Code that Runs the Window
 game.mainloop()
