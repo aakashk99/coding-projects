819beb097380b372ad0b905f423238bb501932e0
Aakash Karlekar <ak@MacBook-Pro.local>
2023-07-11 16:46:16 -0400

    Add documentation to Queue Class

--- Data-Structures/Queue.java
+++ Data-Structures/Queue.java
@@ -11,19 +11,34 @@ import java.util.ArrayList;
 public class Queue<T> {
     private ArrayList<T> list;
 
+    /**
+     * No-Argument Constructor that instantiates an Empty Queue
+     */
     public Queue() {
         this.list = new ArrayList<T>();
     }
 
+    /**
+     * One-Arguement Constructor that instantiates a Queue with value as its first element
+     * @param value
+     */
     public Queue(T value) {
         this.list = new ArrayList<T>();
         this.list.add(value);
     }
 
+    /**
+     * Adds value to the back of the queue
+     * @param value
+     */
     public void enqueue(T value) {
         this.list.add(value);
     }
 
+    /**
+     * Dequeues the value at the front of the Queue
+     * @return the first value in the Queue
+     */
     public T poll() {
         if (this.isEmpty()) {
             throw new RuntimeException("The Queue is currently Empty");
@@ -33,18 +48,32 @@ public class Queue<T> {
         return value;
     }
 
+    /**
+     * @return the length of the Queue
+     */
     public int size() {
         return this.list.size();
     }
 
+    /**
+     * @return true if the Queue is empty, false otherwise
+     */
     public boolean isEmpty() {
         return this.size() == 0;
     }
 
+    /**
+     * Helper method used in the overrided equals method 
+     * @return a list representation of the Queue
+     */
     private ArrayList<T> getList() {
         return this.list;
     }
 
+    /**
+     * @param other
+     * @return true if this Queue is equivalent to the other Queue, false otherwise
+     */
     public boolean equals(Queue<T> other) {
         if (this.list.size() != other.size())
             return false;
@@ -55,6 +84,9 @@ public class Queue<T> {
         return true; 
     }
 
+    /**
+     * Creates a String representation of the Queue
+     */
     public String toString() {
         if (this.isEmpty())
             return "Empty";
